aedf7f277e9ef35f128693e262949c2514a14798
Vemuri saketh <155712037+Vemurisaketh@users.noreply.github.com>
2024-01-25 10:47:46 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+import User from "./User";
+function App() {
+    return (
+        <>
+            <User name="XYRAM SOFTWARE SOLUTIONS" para="Xyram was incorporated in 2013 and has been providing specialized Consulting and IT Services in  Healthcare. Since incorporation, with passionate leadership, clear vision, commitment and proven capability, Xyram has grown to become a trusted partner for Healthcare Services and  provider of Enterprise Business Application Services
+" />
+        </>
+
+
+    )
+
+}
+
+export default App;
